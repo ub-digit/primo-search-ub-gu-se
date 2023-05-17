c5f28f0a9c458c205cb7b592e4846413eecb1604
Johan Larsson <karmajohan@gmail.com>
2023-05-17 14:10:42 +0200

    new url

--- search-primo/vite.config.js
+++ search-primo/vite.config.js
@@ -5,8 +5,8 @@ import { loadingScript } from 'vite-plugin-loading-script'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), loadingScript({
-    externalSrc: "https://ub-digit.github.io/primo-search-ub-gu-se/search-primo/dist/assets/", // Will prefix a url to load from.
-    fileName: 'app.5dae9ca5.js.5dae9ca5.js.5781caf9.js.5781caf9.js', // app.[hash].js
+    externalSrc: "https://ub-digit.github.io/primo-search-ub-gu-se/search-primo/dist/", // Will prefix a url to load from.
+    fileName: 'app.5dae9ca5.js.5dae9ca5.js.5781caf9.js.5781caf9.js.42d35c32.js.51825ff9.js', // app.[hash].js
     crossorigin: false, // Will add the 'crossorigin' attribute to the entry script
     //crossoriginVal: string // The value to set for the 'crossorigin' attribute
   })],
--- ub.gu.se.js
+++ ub.gu.se.js
@@ -1,5 +1,5 @@
 (function init() {
-  const src = "search-primo/dist/app.5dae9ca5.js.5dae9ca5.js.5781caf9.js.5781caf9.js.42d35c32.js"
+  const src = "search-primo/dist/app.5dae9ca5.js.5dae9ca5.js.5781caf9.js.5781caf9.js.42d35c32.js.51825ff9.js"
   var s = document.createElement( 'script' );
   s.setAttribute( 'src', src );
   document.body.appendChild( s );
