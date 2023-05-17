0618392af3d0aed7184e0974c2e87784de36df6d
Johan Larsson <karmajohan@gmail.com>
2023-05-17 13:51:23 +0200

    new build

--- search-primo/vite.config.js
+++ search-primo/vite.config.js
@@ -6,7 +6,7 @@ import { loadingScript } from 'vite-plugin-loading-script'
 export default defineConfig({
   plugins: [vue(), loadingScript({
     externalSrc: "https://ub-digit.github.io/primo-search-ub-gu-se/", // Will prefix a url to load from.
-    fileName: 'app.5dae9ca5.js.5dae9ca5.js.5781caf9.js', // app.[hash].js
+    fileName: 'app.5dae9ca5.js.5dae9ca5.js.5781caf9.js.5781caf9.js', // app.[hash].js
     crossorigin: false, // Will add the 'crossorigin' attribute to the entry script
     //crossoriginVal: string // The value to set for the 'crossorigin' attribute
   })],
