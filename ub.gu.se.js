fd100cf1fc224dae1c0fed543b30b7fc39fa8df0
Johan Larsson <karmajohan@gmail.com>
2023-05-17 13:54:31 +0200

    link to new buildfile

--- ub.gu.se.js
+++ ub.gu.se.js
@@ -1,5 +1,5 @@
 (function init() {
-  const src = "search-primo/dist/app.5dae9ca5.js.5dae9ca5.js.5781caf9.js"
+  const src = "search-primo/dist/app.5dae9ca5.js.5dae9ca5.js.5781caf9.js.5781caf9.js"
   var s = document.createElement( 'script' );
   s.setAttribute( 'src', src );
   document.body.appendChild( s );
