2f38e309a933d59c14e843703528b31a783be8ef
Johan Larsson <karmajohan@gmail.com>
2023-05-22 09:13:19 +0200

    absolute url

--- ub.gu.se.js
+++ ub.gu.se.js
@@ -1,5 +1,5 @@
 (function init() {
-  const src = "search-primo/dist/app.5dae9ca5.js.5dae9ca5.js.5781caf9.js.5781caf9.js.42d35c32.js.51825ff9.js"
+  const src = "https://ub-digit.github.io/primo-search-ub-gu-se/search-primo/dist/app.5dae9ca5.js.5dae9ca5.js.5781caf9.js.5781caf9.js.42d35c32.js.51825ff9.js"
   var s = document.createElement( 'script' );
   s.setAttribute( 'src', src );
   document.body.appendChild( s );
